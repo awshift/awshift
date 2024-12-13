df9721e65cac76e298d1ddb1d395e0a63eadf89d
sayk <97113955+saykdoteth@users.noreply.github.com>
2024-12-13 16:06:27 +0100

    dns update

--- terraform/compute/dns.tf
+++ terraform/compute/dns.tf
@@ -31,3 +31,61 @@ resource "aws_route53_record" "awshift" {
 
   records = module.master_instances[0].public_ips[0]
 }
+
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "api.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = module.master_instances[0].public_ips[0]
+}
+
+resource "aws_route53_record" "api-int" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "api-int.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = module.master_instances[0].public_ips[0]
+}
+
+resource "aws_route53_record" "apps" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = ".apps.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = module.master_instances[0].public_ips[0]
+}
+
+resource "aws_route53_record" "bootstrap" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "bootstrap.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = module.master_instances[0].public_ips[0]
+}
+
+resource "aws_route53_record" "master" {
+  for_each = toset(["master1", "master2", "master3"])
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${each.key}.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [element(module.master_instances[each.key == "master1" ? 0 : each.key == "master2" ? 1 : 2].public_ips, 0)]
+}
+
+resource "aws_route53_record" "worker" {
+  for_each = toset(["worker1", "worker2", "worker3"])
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${each.key}.shift.awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [element(module.worker_instances[each.key == "worker1" ? 0 : each.key == "worker2" ? 1 : 2].public_ips, 0)]
+}
